9cc7f70ca202d279e3fc0ed3963e8b5c57589b94
1jerry <1@1jerry.com>
2021-08-20 16:21:31 -0700

    add todo list.  Make store observable. Update README.md

--- src/stores/TodoStore.ts
+++ src/stores/TodoStore.ts
@@ -1,7 +1,14 @@
 import { createContext } from "react"
-import { makeAutoObservable } from "mobx"
+import { action, computed, makeObservable, observable } from "mobx"
 
 export class Todos {
+  constructor() {
+    makeObservable(this, {
+      todos: observable,
+      remainingTodos: computed,
+      toggleTodo: action,
+    })
+  }
   todos = [
     { id: 1, text: "Buy eggs", completed: true },
     { id: 2, text: "Write a post", completed: false },
